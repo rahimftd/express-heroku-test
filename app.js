56a237ef5fb4635c97e84821b245bf60e40c7cfc
Rahim <rahim.dharssi@gmail.com>
2016-03-22 22:49:09 -0700

    Update port and ip in app.js to work with heroku

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var router = require('./routers.js');
 
 // Config variables
-var ip = '127.0.0.1';
-var port = 3000;
+// var ip = '127.0.0.1';
+var port = process.env.PORT || 3000;
 
 // Initialize app
 var app = express();
-app.listen(port, ip);
+app.listen(port);
 
 // Load routers
 app.use(router);
